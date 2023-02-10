36906db85c928ec93f1c86b22531bf989dcd60ef
Belphin <findolph0003@gmail.com>
2023-02-10 16:47:38 +0200

    post's tags

--- server/models/Post.js
+++ server/models/Post.js
@@ -4,6 +4,7 @@ const Post = new Schema({
 	title: { type: String, require: true },
 	body: { type: String, require: true },
 	author: { type: String, require: true },
+	tags: { type: Array },
 });
 
 module.exports = model("Post", Post);
