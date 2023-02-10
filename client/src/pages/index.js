a0c4b7a2145e82079263aeb4fc61794d05015356
Markus Wedler <markuswedler8@gmail.com>
2023-02-10 14:31:41 +1000

    .

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,8 @@
 const Home = () => {
   return(
-    <main className="wrapper">
+    <main className="home wrapper">
       <div className="posts">
-        <a className="post" href="">
+        <a className="post" href={"/post/" + "postID_01"}>
           <h3>Lorem, ipsum.</h3>
           <ul className="hashtags">
             <li>#lorem</li>
@@ -10,7 +10,7 @@ const Home = () => {
           </ul>
           <div className="author">Ekaterine Mitagvaria</div>
         </a>
-        <a className="post" href="">
+        <a className="post" href={"/post/" + "postID_02"}>
           <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero corporis dolorem veniam quod earum voluptate minus dolor, unde possimus fugit?</h3>
           <ul className="hashtags">
             <li>#lorem</li>
@@ -18,7 +18,7 @@ const Home = () => {
           </ul>
           <div className="author">Ekaterine Mitagvaria</div>
         </a>
-        <a className="post" href="">
+        <a className="post" href={"/post/" + "postID_03"}>
           <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi maxime officia earum repellendus autem ipsa odit tempora iure, sed pariatur consectetur ullam error iusto doloremque. Aspernatur iusto, aut quas sit harum eveniet laborum animi quos id. Ducimus nemo excepturi officia dignissimos? Dolorum nulla illo aliquam. Nesciunt repellat eveniet debitis repellendus.</h3>
           <ul className="hashtags">
             <li>#lorem</li>
---
+++ client/src/pages/post/[id].js
@@ -0,0 +1,9 @@
+const Post = () => {
+  return (
+    <main className="post wrapper">
+      <h1>Content will be here</h1>
+    </main>
+  )
+}
+ 
+export default Post
