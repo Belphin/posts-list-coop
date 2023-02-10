dc15bf579a88a52d113502096b05ce0ddf639e6b
Markus Wedler <markuswedler8@gmail.com>
2023-02-10 11:57:34 +1000

    .

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,10 +3,8 @@ import { createStore, combineReducers } from "redux"
 // reducers
 import { loggedReducer } from "./loggedReducer"
 
-const rootReducer = combineReducers(
-  {
-    logged: loggedReducer
-  }
-)
+const rootReducer = combineReducers({
+  logged: loggedReducer
+})
 
 export const store = createStore(rootReducer)
