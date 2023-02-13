6f3ffb741c05556779a629dd7be6cc9da26bba75
Markus Wedler <markuswedler8@gmail.com>
2023-02-13 16:28:02 +1000

    .

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,38 +1,71 @@
 // react
-import { useLayoutEffect, useState } from "react"
+import { useEffect, useState } from "react"
 // next
 import Link from "next/link"
 
 const Home = () => {
-  const [posts, setPosts] = useState()
+  const postsPerPage = 8
+  const [page, setPage] = useState(0)
+  const [maxCount, setMaxCount] = useState()
+  const [posts, setPosts] = useState([])
 
-  const getPosts = async () => {
-    await fetch("http://localhost:8080/api/post", {
-      cache: 'no-store'
-    })
+  const getPosts = async (postNum) => {
+    await fetch("http://localhost:8080/api/post?page="+postNum+"&limit="+postsPerPage, { cache: 'no-store' })
       .then(res => res.json())
-      .then(data => setPosts(data))
+      .then(data => {
+        if(!maxCount) setMaxCount(data.maxCount)
+        setPosts([...posts, ...data.posts])
+      })
   }
 
-  useLayoutEffect(()=>{
-    getPosts()
+  useEffect(()=>{
+    // set an observer
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        let observerIntervar
+        if(entry.isIntersecting) setPage(page => page + .5)
+        ////
+        // let observerIntervar
+        // if(entry.isIntersecting) observerIntervar = setInterval(()=>{setPage(page => page + .5)}, 1000)
+        // else{
+        //   console.log("NOT OBSERVING");
+        //   clearInterval(observerIntervar)
+        // }
+      })
+    })
+
+    // add the observer
+    const observerTrigger = document.querySelector("main.home .posts .observerTrigger")
+    observer.observe(observerTrigger)
   }, [])
+
+  useEffect(()=>{
+    console.log("page: " + page);
+    if(maxCount && (page * postsPerPage - postsPerPage) > maxCount) return
+    if(page > 0) getPosts(page)
+  }, [page])
+
+  // useEffect(()=>{
+  //   console.log(posts);
+  // }, [posts])
 
   return(
     <main className="home wrapper">
       <div className="posts">
-        { !posts && <div>Loading...</div> }
-        {
-          posts && posts.posts.map((post, i) => (
-            <Link className="post" href={"/post/" + post._id} key={i}>
-              <h3>{ post.title }</h3>
-              <ul className="tags">
-                { post.tags && post.tags.map((tag, i) => (<li key={i}>#{ tag }</li>)) }
-              </ul>
-              <div className="author">{ post.author }</div>
-            </Link>
-          ))
+        { posts.length == 0?
+            <div>Loading...</div>
+          :
+            posts.map((post, i) => (
+              <Link className="post" href={"/post/" + post._id} key={i}>
+                <h3>{ post.title }</h3>
+                <ul className="tags">
+                  { post.tags && post.tags.map((tag, i) => (<li key={i}>#{ tag }</li>)) }
+                </ul>
+                <div className="author">{ post.author }</div>
+              </Link>
+            ))
         }
+        <div className="observerTrigger" />
       </div>
     </main>
   )
