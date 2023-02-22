6230fa4d31e40960714e7178b2a7579b1dd10e3d
Markus Wedler <markuswedler8@gmail.com>
2023-02-22 13:05:18 +1000

    .

--- client/src/pages/post/editor/index.js
+++ client/src/pages/post/editor/index.js
@@ -1,63 +1,25 @@
 // react
-import { useState } from "react";
+import { useEffect, useState } from "react"
 
 const New = () => {
-	const tagsNum = 4;
-	const [tags, setTags] = useState(["", "", "", ""]);
-	const allowed = [
-		null,
-		"_",
-		"a",
-		"b",
-		"c",
-		"d",
-		"e",
-		"f",
-		"g",
-		"h",
-		"i",
-		"j",
-		"k",
-		"l",
-		"m",
-		"n",
-		"o",
-		"p",
-		"q",
-		"r",
-		"s",
-		"t",
-		"u",
-		"v",
-		"w",
-		"x",
-		"y",
-		"z",
-		"0",
-		"1",
-		"2",
-		"3",
-		"4",
-		"5",
-		"6",
-		"7",
-		"8",
-		"9",
-	];
+  const [username, setUsername] = useState()
+  const tagsNum = 4
+  const [tags, setTags] = useState(["", "", "", ""])
+  const allowed = [ null, '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
 
 	const submitPost = async (e) => {
-		e.preventDefault();
+		e.preventDefault()
 		// choose only not empty tags
-		const finalTags = [];
-		tags.forEach((tag) => {
-			if (tag) finalTags.push(tag);
-		});
+		const finalTags = []
+		tags.forEach(tag => {
+			if(tag) finalTags.push(tag)
+		})
 		const data = {
 			title: e.target.querySelector("#title").value,
 			tags: finalTags,
 			body: e.target.querySelector("#body").value,
 			author: localStorage.getItem("username"),
-		};
+		}
 		await fetch("http://localhost:8080/api/post", {
 			method: "POST",
 			cache: "no-cache",
@@ -68,59 +30,68 @@ const New = () => {
 			body: JSON.stringify(data),
 		})
 			.then(() => document.querySelector("header .logo").click())
-			.catch((e) => console.log(e.message));
-	};
+	}
+
+  useEffect(()=>{
+    setUsername(localStorage.getItem("username"))
+  }, [])
 
 	return (
-		<main className="editor wrapper">
-			<form onSubmit={submitPost}>
-				<textarea
-					required
-					type="text"
-					className="title"
-					id="title"
-					placeholder="Title..."
-					onChange={(e) => {
-						e.target.style.height = e.target.scrollHeight + "px";
-					}}
-				/>
-				<div className="tags">
-					{[...Array(tagsNum)].map((x, i) => (
-						<div className="field" key={i}>
-							<div className="hashtag">#</div>
-							<input
-								type="text"
-								className={"tag_" + i}
-								placeholder="new tag..."
-								value={tags && tags[i]}
-								onPaste={(e) => {
-									e.preventDefault();
-								}}
-								onChange={(e) => {
-									if (!allowed.includes(e.nativeEvent.data))
-										e.target.value = e.target.value - 1;
-									else {
-										let tmp = [...tags];
-										tmp[i] = e.target.value;
-										setTags(tmp);
-									}
-								}}
-							/>
-						</div>
-					))}
-				</div>
-				<textarea
-					required
-					className="body"
-					id="body"
-					placeholder="Write here..."
-				/>
-				<nav>
-					<button className="btn">Publish</button>
-				</nav>
-			</form>
-		</main>
-	);
-};
+    <>
+      { username?
+          <main className="editor wrapper">
+            <form onSubmit={submitPost}>
+              <textarea
+                required
+                type="text"
+                className="title"
+                id="title"
+                placeholder="Title..."
+                onChange={(e) => {
+                  e.target.style.height = e.target.scrollHeight + "px"
+                }}
+              />
+              <div className="tags">
+                {[...Array(tagsNum)].map((x, i) => (
+                  <div className="field" key={i}>
+                    <div className="hashtag">#</div>
+                    <input
+                      type="text"
+                      className={"tag_" + i}
+                      placeholder="new tag..."
+                      value={tags && tags[i]}
+                      onPaste={(e) => {
+                        e.preventDefault()
+                      }}
+                      onChange={(e) => {
+                        if (!allowed.includes(e.nativeEvent.data))
+                          e.target.value = e.target.value - 1
+                        else {
+                          let tmp = [...tags]
+                          tmp[i] = e.target.value
+                          setTags(tmp)
+                        }
+                      }}
+                    />
+                  </div>
+                ))}
+              </div>
+              <textarea
+                required
+                className="body"
+                id="body"
+                placeholder="Write here..."
+              />
+              <nav>
+                <button className="btn">Publish</button>
+              </nav>
+            </form>
+          </main>
+        :
+          <div className="wrapper">Forbidden 403</div>
+      }
+    </>
+	)
+}
 
-export default New;
+export default New
